9e4459ca664a27070c3a552dd4cdfb524d6f492f
raycast5 <ray.j.colon@gmail.com>
2022-09-02 16:15:49 +0000

    Added file 1

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a binary tree node to the left
+ * binary_tree_insert_right - inserts a binary tree node to the right
  * @parent: a pointer to the parent node of the node to create
  * @value: value of the new node
  * Return: pointer to new node or NULL on failure.
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
