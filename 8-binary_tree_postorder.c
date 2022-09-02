5c67364475417b35c1604a36a0e7f9a294266329
raycast5 <ray.j.colon@gmail.com>
2022-09-02 21:18:13 +0000

    Added file 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - goes through binary tree using post-order traversal
+ * @tree: binary tree to traverse
+ * @func: function to print contents of tree
+ * Return: Nothing
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+
+	binary_tree_postorder(tree->right, func);
+
+	func(tree->n);
+}
