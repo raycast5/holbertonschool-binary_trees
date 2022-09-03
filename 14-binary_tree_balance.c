ee579382d6f54daee338bcc40cf773da69df7e53
raycast5 <ray.j.colon@gmail.com>
2022-09-03 01:25:12 +0000

    Added helper function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,27 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - finds the height of a binary tree
+ * @tree: a node in a binary tree
+ * Return: height of tree, NULL if it fails
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lheight = 0, rheight = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		lheight = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		rheight = binary_tree_height(tree->right) + 1;
+
+	if (lheight > rheight)
+		return (lheight);
+	else
+		return (rheight);
+}
 
 /**
  * binary_tree_balance - finds balance factor of a binary tree
@@ -14,11 +37,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		lheight = binary_tree_balance(tree->left) + 1;
+		lheight = binary_tree_height(tree->left) + 1;
 	else
 		lheight = 0;
 	if (tree->right)
-		rheight = binary_tree_balance(tree->right) + 1;
+		rheight = binary_tree_height(tree->right) + 1;
 	else
 		rheight = 0;
 
