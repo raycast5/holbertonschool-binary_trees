5a6feff3c3cdceaffe35aeec8a2b8742888d4469
raycast5 <ray.j.colon@gmail.com>
2022-09-03 00:31:26 +0000

    Added file 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts nodes with at least one child
+ * @tree: binary tree
+ * Return: number of nodes with a child
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	return (binary_tree_nodes(tree->left)
+		+ binary_tree_nodes(tree->right) + 1);
+
+}
